c6c2e1e73dd84395d695dbf011bac77e1dbea565
Akram Khalid <akram@akrams-imac.home>
2019-10-20 00:52:55 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <span className="text">Don't expect too much, enjoy what you have.</span>
+      </div>
+      <div className="white"></div>
+      <div className="black"></div>
     </div>
   );
 }
